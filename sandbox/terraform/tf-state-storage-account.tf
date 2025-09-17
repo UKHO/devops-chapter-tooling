2457119d2818781eb9158527eef857672f7f387e
srjames-ukho <126559680+srjames-ukho@users.noreply.github.com>
2025-09-17 13:45:15 +0100

    Add in the storage account information

---
+++ sandbox/terraform/tf-state-storage-account.tf
@@ -0,0 +1,14 @@
+// https://registry.terraform.io/modules/Azure/avm-res-storage-storageaccount/azurerm/0.6.4
+module "avm-res-storage-storageaccount" {
+  source              = "Azure/avm-res-storage-storageaccount/azurerm"
+  version             = "0.6.4"
+  location            = data.azurerm_resource_group.m_devopschapter_rg.location
+  name                = var.tf_state_sa_name
+  resource_group_name = data.azurerm_resource_group.m_devopschapter_rg.name
+
+  containers = {
+    tfstate = {
+      name = "tfstate"
+    }
+  }
+}
