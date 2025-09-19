d95c1631f444a905205efbe493ab141092213f1c
srjames-ukho <126559680+srjames-ukho@users.noreply.github.com>
2025-09-19 13:09:37 +0100

    Add GUID validation for subscription_id variable

--- sandbox/prerequisites/variables.tf
+++ sandbox/prerequisites/variables.tf
@@ -5,4 +5,8 @@ variable "tf_state_sa_name" {
 variable "subscription_id" {
   type        = string
   description = "Subscription where the infrastructure is being deployed to."
+  validation {
+    condition     = can(regex("^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[1-5][0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$", var.subscription_id))
+    error_message = "Must be a valid GUID."
+  }
 }
