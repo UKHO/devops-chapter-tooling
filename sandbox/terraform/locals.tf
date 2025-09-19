f2d6589e3e87759837c446e9e74cdb7ef66f97cd
srjames-ukho <126559680+srjames-ukho@users.noreply.github.com>
2025-09-19 12:01:01 +0100

    Add local tags to sandbox Terraform configuration

---
+++ sandbox/terraform/locals.tf
@@ -0,0 +1,10 @@
+locals {
+  tags = {
+    "CALLOUT_TEAM"     = "On-Call_N/A"
+    "COST_CENTRE"      = "O694850"
+    "ENVIRONMENT"      = "Discovery"
+    "RESPONSIBLE_TEAM" = "DDC"
+    "SERVICE"          = "DevOpsChapter-TestingSpace"
+    "SERVICE_OWNER"    = "Scott James"
+  }
+}
