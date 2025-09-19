d96561fc2080b8df595db33498597a5390c19106
srjames-ukho <126559680+srjames-ukho@users.noreply.github.com>
2025-09-19 10:44:41 +0100

    Remove commented-out validation from tf_state_sa_name variable

--- sandbox/terraform/variables.tf
+++ sandbox/terraform/variables.tf
@@ -1,8 +1,4 @@
 variable "tf_state_sa_name" {
   type        = string
   description = "Storage account name for storing .tfstate"
-  //  validation {
-  //    condition     = can(regex("^[a-zA-Z0-9]{4,20}$", var.tf_state_sa_name))
-  //    error_message = "Value must be 4-20 characters long and only contain alphanumeric characters."
-  //  }
 }
