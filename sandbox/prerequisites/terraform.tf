7f6bc4709fa6bf642631f7ebdee3a6ef268e8118
sola-ukho <asola@ukho.gov.uk>
2025-11-28 00:42:42 +0000

    commit with lint errors

--- sandbox/prerequisites/terraform.tf
+++ sandbox/prerequisites/terraform.tf
@@ -3,12 +3,12 @@ terraform {
 
   required_providers {
     azurerm = {
-      source  = "hashicorp/azurerm"
+      source = "hashicorp/azurerm"
       version = ">= 3.7.0, < 5.0.0"
     }
   }
 
   backend "local" {
-    path = "state/terraform.tfstate"
+    path ="state/terraform.tfstate"
   }
 }
